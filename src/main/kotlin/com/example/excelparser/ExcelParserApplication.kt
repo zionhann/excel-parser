d4ab3cedf634974d518f6e51006489962cf2e037
zionhann <hanzion1108@gmail.com>
2023-08-21 14:47:42 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/excelparser/ExcelParserApplication.kt
@@ -0,0 +1,11 @@
+package com.example.excelparser
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ExcelParserApplication
+
+fun main(args: Array<String>) {
+    runApplication<ExcelParserApplication>(*args)
+}
