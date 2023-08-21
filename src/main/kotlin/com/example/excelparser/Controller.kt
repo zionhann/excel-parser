330b044ff0c948455368968340ea3c34b3d6ce11
zionhann <hanzion1108@gmail.com>
2023-08-21 17:34:30 +0900

    Implement downloading data as ms excel file

--- src/main/kotlin/com/example/excelparser/Controller.kt
+++ src/main/kotlin/com/example/excelparser/Controller.kt
@@ -1,9 +1,14 @@
-package com.example.excelparser;
+package com.example.excelparser
 
+import org.springframework.core.io.Resource
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.multipart.MultipartFile
 
 @Controller
@@ -11,7 +16,7 @@ class Controller(
     private val service: Service,
 ) {
 
-    @PostMapping("/results")
+    @PostMapping("/upload")
     fun upload(@RequestParam file: MultipartFile, model: Model): String {
         val res = service.excelToModel(
             file,
@@ -20,5 +25,20 @@ class Controller(
         model.addAttribute("data", res)
 
         return "index"
+    }
+
+    @GetMapping("/download")
+    @ResponseBody
+    fun download(): ResponseEntity<Resource> {
+        val resource = service.modelToExcel()
+
+        return ResponseEntity.ok()
+            .header(
+                "Content-Disposition",
+                "attachment;filename=example.xlsx"
+            )
+            .contentLength(resource.contentLength())
+            .contentType(MediaType.APPLICATION_OCTET_STREAM)
+            .body(resource)
     }
 }
--- src/main/kotlin/com/example/excelparser/Service.kt
+++ src/main/kotlin/com/example/excelparser/Service.kt
@@ -1,8 +1,12 @@
 package com.example.excelparser
 
+import org.apache.poi.hssf.usermodel.HSSFWorkbook
 import org.apache.poi.ss.usermodel.WorkbookFactory
+import org.springframework.core.io.ByteArrayResource
+import org.springframework.core.io.Resource
 import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
+import java.io.ByteArrayOutputStream
 
 @Service
 class Service(
@@ -26,5 +30,31 @@ class Service(
         repository.saveAll(models)
 
         return models
+    }
+
+    fun modelToExcel(): Resource {
+        val all = repository.findAll()
+
+        val wb = HSSFWorkbook()
+        val sheet = wb.createSheet()
+        val header = sheet.createRow(0)
+
+        header.createCell(0).setCellValue("번호")
+        header.createCell(1).setCellValue("이름")
+        header.createCell(2).setCellValue("설명")
+
+        all.forEachIndexed { index, model ->
+            val row = sheet.createRow(index + 1)
+
+            row.createCell(0).setCellValue(model.id.toString())
+            row.createCell(1).setCellValue(model.name)
+            row.createCell(2).setCellValue(model.description)
+        }
+
+        ByteArrayOutputStream().use { stream ->
+            wb.write(stream)
+            wb.close()
+            return ByteArrayResource(stream.toByteArray())
+        }
     }
 }
